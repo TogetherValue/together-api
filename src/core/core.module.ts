35a6f3bd5859dc00c730b136f68866a26ef198bd
wonjjee <yoteamo7@naver.com>
2024-02-28 00:17:57 +0900

    faet: added WinstonConfigService

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,12 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { getTypeOrmModule } from './database/typeorm/typeorm.module';
+import { WinstonConfigService } from './logger/winston-config.service';
+import { WinstonModule } from 'nest-winston';
 
 const modules = [ConfigModule];
 
 @Global()
 @Module({
-  imports: [getTypeOrmModule(), ...modules],
+  imports: [
+    getTypeOrmModule(),
+    WinstonModule.forRootAsync({ useClass: WinstonConfigService }),
+    ...modules,
+  ],
   providers: [],
   exports: [...modules],
 })
--- src/setNestApp.ts
+++ src/setNestApp.ts
@@ -4,8 +4,10 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 export function setNestApp<T extends INestApplication>(app: T): void {
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(
