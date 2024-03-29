16bbf266bb09d6096436f1a12da63f5627620507
wonjjee <yoteamo7@naver.com>
2024-03-29 13:50:56 +0900

    feat: added ipMiddleware

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,14 +1,36 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PostRepositoryModule } from 'src/entities/post/post-repository.module';
 import { PostController } from './controller/post.controller';
 import { PostService } from './service/post.service';
 import { HttpModule } from '@nestjs/axios';
 import { MetaDataExtractor } from 'src/common/util/metaDataExtractor';
 import { ScrapRepositoryModule } from 'src/entities/scrap/scrap-repository.module';
+import { IpMiddleware } from 'src/core/middleware/ip.middleware';
 
 @Module({
   imports: [HttpModule, PostRepositoryModule, ScrapRepositoryModule],
   controllers: [PostController],
   providers: [PostService, MetaDataExtractor],
 })
-export class PostModule {}
+export class PostModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(IpMiddleware)
+      .exclude(
+        {
+          path: 'posts',
+          method: RequestMethod.POST,
+        },
+        {
+          path: 'posts',
+          method: RequestMethod.DELETE,
+        },
+      )
+      .forRoutes(PostController);
+  }
+}
