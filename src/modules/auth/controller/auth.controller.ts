08939715e1e6eb494d2e134e23e1a54cfcbaae8c
wonjjee <yoteamo7@naver.com>
2024-04-01 12:55:50 +0900

    fix: update refreshToken

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,25 +1,50 @@
-import { Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { GetGithubInfoQueryDto } from 'src/common/request/auth/get-githubInfo.dto';
 import { RefreshTokenGuard } from 'src/core/guard/refreshToken.guard';
 import { IValidatedRefreshToken } from 'src/common/types/jwt';
 import { User } from 'src/core/decorator/user.decorator';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('github')
-  async githubLogin(@Query() getGithubInfoQueryDto: GetGithubInfoQueryDto) {
-    return this.authService.githubLogin(getGithubInfoQueryDto);
+  async githubLogin(
+    @Query() getGithubInfoQueryDto: GetGithubInfoQueryDto,
+    @Req() req: Request,
+  ) {
+    const tokens = await this.authService.githubLogin(getGithubInfoQueryDto);
+
+    req.res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+    });
+    return { accessToken: tokens.accessToken };
   }
 
   @UseGuards(RefreshTokenGuard)
   @Patch('refresh')
-  async refreshToken(@User() user: IValidatedRefreshToken) {
+  async refreshToken(
+    @User() user: IValidatedRefreshToken,
+    @Req() req: Request,
+  ) {
     const userId = user['sub'];
     const refreshToken = user['refreshToken'];
 
-    return this.authService.refreshTokens(userId, refreshToken);
+    const tokens = await this.authService.refreshTokens(userId, refreshToken);
+    req.res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+    });
+
+    return { accessToken: tokens.accessToken };
   }
 }
--- src/setNestApp.ts
+++ src/setNestApp.ts
@@ -5,8 +5,10 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import cookieParser from 'cookie-parser';
 
 export function setNestApp<T extends INestApplication>(app: T): void {
+  app.use(cookieParser());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
